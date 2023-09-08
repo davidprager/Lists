7c145b750a22825d87d836119f2480a5bdaf18af
davidprager <david_prager@yahoo.com>
2023-09-07 20:15:29 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,42 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #8, functions module
+Your name here
+"""
+
+# This module contains a series of functions that implement Python list functionality
+
+# function:    list_len
+# INPUT:       a list
+# PROCESSING:  determines the size of the list
+# OUTPUT:      an integer representing the size of the list
+
+
+# function:    list_max
+# INPUT:       a list
+# PROCESSING:  obtains the largest element in the list
+# OUTPUT:      returns the largest element in the list
+
+
+# function:    list_copy
+# INPUT:       a list
+# PROCESSING:  creates a new list which serves as a copy of the supplied list
+# OUTPUT:      returns a new copy of the list
+
+
+# function:    list_append
+# INPUT:       a list and an element to add to the list (any data type)
+# PROCESSING:  creates a new list which includes the new element appended
+#              to the end of the list
+# OUTPUT:      returns a new copy of the list
+
+
+# function:    list_insert
+# INPUT:       a list, an index position (integer) and a data
+#              element (can be a string, float, Boolean or
+#              int)
+# PROCESSING:  inserts the supplied data element into the
+#              list at the desired index position.  If the list is shorter than the
+#              supplied index position, add the new element to the end of the list.
+# OUTPUT:      return a new copy of the list that contains
+#              the inserted element
---
+++ program2.py
@@ -0,0 +1,7 @@
+"""
+CSCI-UA.0002 Spring 2024
+Assignment #8, Program 2
+Your name here
+"""
+
+# This program provides a management tool for an auto parts store
